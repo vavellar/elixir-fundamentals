f28289a5282a448f6119f00d4d2894a09efdbe6e
vavellar <victor.avellar01@gmail.com>
2022-10-17 21:58:41 -0300

    default parameters

--- sample_enum.exs
+++ sample_enum.exs
@@ -3,6 +3,17 @@ defmodule Sample.Enum do
         hd(list)
     end
 
-    def first2([]), do: nil
-    def first2([head | _]), do: head
+    def first2(list, val \\ nil)
+    def first2([head | _], _), do: head
+    # Pattern matching
+    def first2([], val), do: val
+
+    #Guard Clauses
+    # def first2(list) when length(list) == 0, do: nil
+
+
+    def add(list, val \\ 0 ) do
+      [val | list]
+    end
+
 end
